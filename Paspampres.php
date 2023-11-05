422965d97edfcf2167d82db4940aac7a55b1eb4b
Muhammad Fathurraihan Saputra <fthrn.s14@gmail.com>
2023-11-05 21:39:26 +0700

    dev a index

--- Paspampres.php
+++ Paspampres.php
@@ -1,12 +1,12 @@
 <?php
-require_once_DIR_ ."/Presiden.php";
-require_once_DIR_."/WakilPresiden.php";
+require_once __DIR__ ."/Presiden.php";
+require_once __DIR__ ."/WakilPresiden.php";
 
     class Paspampres implements Presiden, WakilPresiden{
       public function presiden($nama):void{
-        echo "tugas saja menjaga Presiden $nama";
+        echo "tugas saya menjaga Presiden $nama";
       }
       public function wakilpresiden($nama): void{
-        echo "tugas saja menjaga Wakil Presiden $nama";
+        echo "tugas saya menjaga Wakil Presiden $nama";
       }
 }
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    require_once __DIR__ . "/Paspampres.php";
+
+    $paspampres1 = new Paspampres();
+
+    $paspampres1->presiden("Bowo");
+    echo "<br>";
+    $paspampres1->wakilPresiden("Chandra");
+?>
